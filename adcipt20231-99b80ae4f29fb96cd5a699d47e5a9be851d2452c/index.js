607657e01f3ae17087e75539296fc50189e2addb
Filipi Virgilio <filipi.virgilio@gmail.com>
2023-04-10 14:04:52 +0000

    teste

--- adcipt20231-99b80ae4f29fb96cd5a699d47e5a9be851d2452c/index.js
+++ adcipt20231-99b80ae4f29fb96cd5a699d47e5a9be851d2452c/index.js
@@ -7,6 +7,10 @@ class Game extends Phaser.Game {
   constructor() {
     super(config);
     this.socket = io();
+this.socket.on("connect", (socket) => {
+  this.socket.emit("registro", socket.id)
+})
+
     this.scene.add("abertura", abertura);
     this.scene.add("principal", principal);
     this.scene.add("encerramento", encerramento);
