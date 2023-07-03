d3f9b2249fa993e109de3a591950ca06b83c1165
Filipi Virgilio <filipi.virgilio@gmail.com>
2023-07-03 12:29:18 +0000

    att

--- jogo/index.js
+++ jogo/index.js
@@ -10,30 +10,53 @@ class Game extends Phaser.Game {
   constructor() {
     super(config);
 
-    this.socket = io();  //escolha de caminho
 
-    this.socket.on("connect", () => {
-      this.socket.emit("registro", this.socket.id);
-    });
-
-    this.socket.on("registro_aceito", () => {
-      this.registro = true;
-    });
-
-    this.socket.on("registro_negado", () => {
-      this.registro = false;
-    });
-
-    this.ice_servers = {
-      iceServers: [
-        {
-          urls: "stun:stun.l.google.com:19302",
-        },
-      ],
-    };
-    this.audio = document.querySelector("audio");
-    this.midias = undefined;
-
+     /* URL e lista de STUN/TURN/ICE por provedor */
+     let iceServers
+     if (window.location.host === 'ifsc.digital') {
+       /* Sinalização, escolha de caminho e mídia */
+       this.socket = io.connect({ path: '/SMU2023.1/socket.io/' })
+ 
+       iceServers = [
+         {
+           urls: 'stun:ifsc.digital'
+         },
+         {
+           urls: 'turns:ifsc.digital',
+           username: 'adcipt',
+           credential: 'adcipt20231'
+         },
+         {
+           urls: 'stun:stun.l.google.com:19302'
+         }
+       ]
+     } else {
+       
+      /* Sinalização, escolha de caminho e mídia */
+       this.socket = io()
+ 
+       iceServers = [
+         {
+           urls: 'stun:stun.l.google.com:19302'
+         }
+       ]
+     }
+     this.ice_servers = { iceServers }
+     this.audio = document.querySelector('audio')
+     this.midias = undefined
+ 
+     this.socket.on('connect', () => {
+       this.socket.emit('registro', this.socket.id)
+     })
+ 
+     this.socket.on('registro_aceito', () => {
+       this.registro = true
+     })
+ 
+     this.socket.on('registro_negado', () => {
+       this.registro = false
+     })
+ 
     /* Cenas */
     //this.scene.add("abertura", abertura);
     //this.scene.add("principal", principal);
@@ -43,9 +66,11 @@ class Game extends Phaser.Game {
     this.scene.add("registro", registro);
     this.scene.add("presença", presenca);
     this.scene.start("registro");
-  }
-}
+   }
+ }
+ 
+ window.onload = () => {
+   window.game = new Game()
+ };
 
-window.onload = () => {
-  window.game = new Game();
-};
+
